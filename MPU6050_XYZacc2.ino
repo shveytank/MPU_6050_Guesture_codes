d87d56d824f750237e97f93973bd03b459d50ad2
shveytank <shubhamshukla167@gmail.com>
2020-04-15 11:33:58 +0530

    Add files via upload

---
+++ MPU6050_XYZacc2.ino
@@ -0,0 +1,54 @@
+
+#include "Wire.h"       
+#include "I2Cdev.h"     
+#include "MPU6050.h"    
+
+MPU6050 mpu;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+struct MyData {
+  byte X;
+  byte Y;
+};
+
+MyData data;
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+  mpu.initialize();
+  //pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  data.X = map(ax, -17000, 17000, 0, 255 ); // X axis data
+  data.Y = map(ay, -17000, 17000, 0, 255);  // Y axis data
+  delay(500);
+  Serial.print("Axis X = ");
+  Serial.print(data.X);
+  Serial.print("  ");
+  Serial.print("Axis X = ");
+  Serial.println(data.X);
+   if (data.Y < 80) { //gesture : down 
+    Serial.println("gesture 1");
+    //digitalWrite(LED_BUILTIN, LOW);
+    }
+ if (data.Y > 145) {//gesture : up
+  //digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("gesture 2");
+    }
+ if (data.X > 155) {//gesture : left
+  Serial.println("gesture 3");
+    }
+ if (data.X < 80) {//gesture : right
+  Serial.println("gesture 4");
+    }
+ if (data.X > 100 && data.X < 170 && data.Y > 80 && data.Y < 130) { //gesture : little bit down
+    Serial.println("gesture 5");
+    }
+
+}
